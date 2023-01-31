7b2bc49c17e5f5874c167d644cc08d01be998b79
hoseon <chs1025@gmail.com>
2023-01-31 15:08:03 +0900

    Initial Commit

---
+++ AM5/AM5App.swift
@@ -0,0 +1,17 @@
+//
+//  AM5App.swift
+//  AM5
+//
+//  Created by HoSeon Chu on 2023/01/31.
+//
+
+import SwiftUI
+
+@main
+struct AM5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
